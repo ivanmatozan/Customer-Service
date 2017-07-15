29308637d240220c356961e0469cde1af0511211
Ivan Matozan <ivan.matozan@gmail.com>
2017-07-15 11:16:12 +0200

    Apply middleware on routes

--- app/routes.php
+++ app/routes.php
@@ -2,36 +2,67 @@
 
 $app->get('/', 'App\Controllers\HomeController:index')->setName('home');
 
-$app->get('/register', 'App\Controllers\UserController:getRegister')->setName('user.register');
-$app->post('/register', 'App\Controllers\UserController:postRegister');
-$app->get('/login', 'App\Controllers\UserController:getLogin')->setName('user.login');
-$app->post('/login', 'App\Controllers\UserController:postLogin');
-$app->get('/logout', 'App\Controllers\UserController:logout')->setName('user.logout');
-$app->get('/profile', 'App\Controllers\UserController:profile')->setName('user.profile');
-$app->get('/profile/edit/{id}', 'App\Controllers\UserController:getEdit')->setName('user.edit');
-$app->post('/profile/edit/{id}', 'App\Controllers\UserController:postEdit');
+// Logged in users can't access login and register forms
+$app->group('', function () {
+    $this->get('/register', 'App\Controllers\UserController:getRegister')->setName('user.register');
+    $this->post('/register', 'App\Controllers\UserController:postRegister');
+    $this->get('/login', 'App\Controllers\UserController:getLogin')->setName('user.login');
+    $this->post('/login', 'App\Controllers\UserController:postLogin');
+})->add(new \App\Middleware\Auth\GuestMiddleware($container));
 
-$app->get('/user-management', 'App\Controllers\UserManagementController:show')->setName('user-management.show');
-$app->get('/user-management/create', 'App\Controllers\UserManagementController:getCreate')->setName('user-management.create');
-$app->post('/user-management/create', 'App\Controllers\UserManagementController:postCreate');
-$app->get('/user-management/edit/{id}', 'App\Controllers\UserManagementController:getEdit')->setName('user-management.edit');
-$app->post('/user-management/edit/{id}', 'App\Controllers\UserManagementController:postEdit');
-$app->get('/user-management/delete/{id}', 'App\Controllers\UserManagementController:confirmDelete')->setName('user-management.delete');
-$app->post('/user-management/delete/{id}', 'App\Controllers\UserManagementController:delete');
+// Only logged in user can logout and access profile view
+$app->group('', function () {
+    $this->get('/logout', 'App\Controllers\UserController:logout')->setName('user.logout');
+    $this->get('/profile', 'App\Controllers\UserController:profile')->setName('user.profile');
+    $this->get('/profile/edit/{id}', 'App\Controllers\UserController:getEdit')->setName('user.edit');
+    $this->post('/profile/edit/{id}', 'App\Controllers\UserController:postEdit');
+})->add(new \App\Middleware\Auth\AuthMiddleware($container));
 
-$app->get('/questions', 'App\Controllers\QuestionController:list')->setName('question.list');
-$app->get('/questions/create', 'App\Controllers\QuestionController:getCreate')->setName('question.create');
-$app->post('/questions/create', 'App\Controllers\QuestionController:postCreate');
-$app->get('/questions/edit/{id}', 'App\Controllers\QuestionController:getEdit')->setName('question.edit');
-$app->post('/questions/edit/{id}', 'App\Controllers\QuestionController:postEdit');
-$app->get('/questions/delete/{id}', 'App\Controllers\QuestionController:confirmDelete')->setName('question.delete');
-$app->post('/questions/delete/{id}', 'App\Controllers\QuestionController:delete');
-$app->get('/questions/close/{id}', 'App\Controllers\QuestionController:confirmClose')->setName('question.close');
-$app->post('/questions/close/{id}', 'App\Controllers\QuestionController:close');
-$app->get('/questions/show/{id}', 'App\Controllers\QuestionController:show')->setName('question.show');
+// Only webadmin can access user-management
+$app->group('/user-management', function () {
+    $this->get('', 'App\Controllers\UserManagementController:show')->setName('user-management.show');
+    $this->get('/create', 'App\Controllers\UserManagementController:getCreate')->setName('user-management.create');
+    $this->post('/create', 'App\Controllers\UserManagementController:postCreate');
+    $this->get('/edit/{id}', 'App\Controllers\UserManagementController:getEdit')->setName('user-management.edit');
+    $this->post('/edit/{id}', 'App\Controllers\UserManagementController:postEdit');
+    $this->get('/delete/{id}', 'App\Controllers\UserManagementController:confirmDelete')->setName('user-management.delete');
+    $this->post('/delete/{id}', 'App\Controllers\UserManagementController:delete');
+})->add(new \App\Middleware\Role\WebAdminMiddleware($container))
+    ->add(new \App\Middleware\Auth\AuthMiddleware($container));
 
-$app->post('/questions/{id}/response', 'App\Controllers\ResponseController:postCreate')->setName('response.create');
-$app->get('/response/edit/{id}', 'App\Controllers\ResponseController:getEdit')->setName('response.edit');
-$app->post('/response/edit/{id}', 'App\Controllers\ResponseController:postEdit');
-$app->get('/response/delete/{id}', 'App\Controllers\ResponseController:confirmDelete')->setName('response.delete');
-$app->post('/response/delete/{id}', 'App\Controllers\ResponseController:delete');
+// Only admin and user can see question(s)
+$app->group('/questions', function () {
+    $this->get('', 'App\Controllers\QuestionController:list')->setName('question.list');
+    $this->get('/show/{id}', 'App\Controllers\QuestionController:show')->setName('question.show');
+})->add(new \App\Middleware\Role\UserMiddleware($container))
+    ->add(new \App\Middleware\Role\AdminMiddleware($container))
+    ->add(new \App\Middleware\Auth\AuthMiddleware($container));
+
+// Only user can create new question
+$app->group('/questions', function () {
+    $this->get('/create', 'App\Controllers\QuestionController:getCreate')->setName('question.create');
+    $this->post('/create', 'App\Controllers\QuestionController:postCreate');
+})->add(new \App\Middleware\Role\UserMiddleware($container))
+    ->add(new \App\Middleware\Auth\AuthMiddleware($container));
+
+// Only user can edit|delete|close question
+$app->group('/questions', function () {
+    $this->get('/edit/{id}', 'App\Controllers\QuestionController:getEdit')->setName('question.edit');
+    $this->post('/edit/{id}', 'App\Controllers\QuestionController:postEdit');
+    $this->get('/delete/{id}', 'App\Controllers\QuestionController:confirmDelete')->setName('question.delete');
+    $this->post('/delete/{id}', 'App\Controllers\QuestionController:delete');
+    $this->get('/close/{id}', 'App\Controllers\QuestionController:confirmClose')->setName('question.close');
+    $this->post('/close/{id}', 'App\Controllers\QuestionController:close');
+})->add(new \App\Middleware\Role\UserMiddleware($container))
+    ->add(new \App\Middleware\Auth\AuthMiddleware($container));
+
+// Only admin and user can create|edit|delete response
+$app->group('', function () {
+    $this->post('/questions/{id}/response', 'App\Controllers\ResponseController:postCreate')->setName('response.create');
+    $this->get('/response/edit/{id}', 'App\Controllers\ResponseController:getEdit')->setName('response.edit');
+    $this->post('/response/edit/{id}', 'App\Controllers\ResponseController:postEdit');
+    $this->get('/response/delete/{id}', 'App\Controllers\ResponseController:confirmDelete')->setName('response.delete');
+    $this->post('/response/delete/{id}', 'App\Controllers\ResponseController:delete');
+})->add(new \App\Middleware\Role\UserMiddleware($container))
+    ->add(new \App\Middleware\Role\AdminMiddleware($container))
+    ->add(new \App\Middleware\Auth\AuthMiddleware($container));
